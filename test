49922f23b972ea623e862537445a5fcce6e99633
Rima Mehra <rimamehra@MacBook-Pro.attlocal.net>
2020-10-16 23:29:19 -0500

    Adding the election audit code.

---
+++ test
@@ -0,0 +1,3 @@
+numbers = [0, 1, 2, 3, 4]
+for num in numbers:
+    print(num)
---
+++ test.py
@@ -0,0 +1,48 @@
+# counties_tuple = ("Arapahoe","Denver","Jefferson")
+# for county in counties_tuple:
+#     print(county)
+
+# voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+#                 {"county":"Denver", "registered_voters": 463353},
+#                 {"county":"Jefferson", "registered_voters": 432438}]
+
+# for county_dict in voting_data:
+#     print(county_dict['county'])
+
+# my_votes = int(input("How many votes did you get in the election? "))
+# total_votes = int(input("What is the total votes in the election? "))
+# percentage_votes = my_votes/total_votes *100
+# print("I received " + str(percentage_votes)+"% of the total votes.")
+
+# my_votes = int(input("How many votes did you get in the election? "))
+# total_votes = int(input("What is the total votes in the election? "))
+# print(f"I received{my_votes / total_votes * 100}% of the total votes.")
+
+# counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+# for county, voters in counties_dict.items():
+#     print(county + " county has " + str(voters) + " registered voters.")
+
+# counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+# for county, voters in counties_dict.items():
+#     print(f"{county} county has {voters} registered voters.")
+
+
+# candidate_votes = int(input("How many votes did the candidate get in the election? "))
+# total_votes = int(input("What is the total number of votes in the election? "))
+# message_to_candidate = (
+#     f"You received {candidate_votes} number of votes. "
+#     f"The total number of votes in the election was {total_votes}. "
+#     f"You received {candidate_votes / total_votes * 100}% of the total votes.")
+
+# print(message_to_candidate)
+
+# counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+# for county, voters in counties_dict.items():
+#     print(f"{county} county has {voters} registered voters")
+
+# voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, {"county":"Denver", "registered_voters": 463353}, {"county":"Jefferson", "registered_voters": 432438}]
+# for county_dict in voting_data
+#     for registered_voters in voting_data:
+#         print(f"{county_dict} county has {registered_voters} registered voters")
+    
+
